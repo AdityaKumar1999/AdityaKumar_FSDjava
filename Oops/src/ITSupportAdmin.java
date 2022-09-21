0acd8ab9eb7ef475f5b256324ac948b721515584
ADITYA KUMAR <ADITYA KUMAR@STEVEGLOCK>
2022-09-21 22:51:37 +0530

    1st submissions

--- Oops/src/ITSupportAdmin.java
+++ Oops/src/ITSupportAdmin.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Random;
-class Employee{
+class Employee{						//Sub Class
 		String FirstName;
 		String LastName;
 		
@@ -12,18 +12,29 @@ class Employee{
 			this.LastName = LastName;
 		}
 } 
-public class ITSUPPPORTADMIN {
+public class ITSupportAdmin {						//Main Class
 	public static void main(String args[]) {
 		System.out.println("Please Enter Your Full Name");
 		Scanner sc = new Scanner(System.in);
 		String firstName = sc.nextLine();
 		String lastName = sc.nextLine();
-		Employee e = new Employee(firstName,lastName);
+		while(!firstName.matches("[a-zA-Z ,]+")||(!lastName.matches("[a-zA-Z ,]+"))) { //Checking Name(To prevent integer type input)
+			System.out.println("Please Retype your Name ");
+			firstName = sc.nextLine();
+			lastName = sc.nextLine();
+		} while(firstName.length()<0 && firstName.length()>8 || lastName.length()<0 && lastName.length()>8){  //Checking Name Length
+			System.out.println("Please Retype your Name ");
+			firstName = sc.nextLine();
+			lastName = sc.nextLine();
+		} 
+		Employee e = new Employee(firstName, lastName);
 		e.EmpInfo();
 		System.out.println("Please Choose Your Department\n1. Technical\n2. Admin\n3. Human Resource\n4. Legal");
 		int department = sc.nextInt();
-		System.out.println(department);
-		System.out.println("Dear "+firstName+" "+lastName+" your generated credentials are as follows");
+		while(department<1 || department>4) {    //for Choosing only Valid Option 
+		System.out.println("Please Choose Your Department from Below Mentioned List only \n1. Technical\n2. Admin\n3. Human Resource\n4. Legal");
+		department = sc.nextInt();
+		} System.out.println("Dear "+firstName+" "+lastName+" your generated credentials are as follows");
 		if(department==1) {
 		System.out.println("Email ---> "+firstName+lastName+"@Technical.GreatLearing.com");
 		} else if (department==2) {
@@ -32,13 +43,11 @@ public class ITSUPPPORTADMIN {
 		System.out.println("Email ---> "+firstName+lastName+"@Human_Resource.GreatLearing.com");
 		} else if (department==4) {
 		System.out.println("Email ---> "+firstName+lastName+"@Legal.GreatLearing.com");
-		} else {
-		System.out.println("Please Enter a Valid Departmet");
-		}
-	    String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		} 
+		String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String  lowercase = "abcdefghijklmnopqrstuvwxyz";
 		String num = "0123456789";
-		String specialchars = "!@#$%^&*()_-+=[]\',./*-+{}|:<>?";
+		String specialchars = "!@#$%^&()_-+=[]\',./-+{}|:<>?";
 		String combination = uppercase+lowercase+specialchars+num;
 		int len = 10;
 		char[] password = new char[len];
